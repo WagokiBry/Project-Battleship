72241a2ee4cfd2601120c79ee5a1c8c824a87dec
Bryan <bryanwagoki2016@gmail.com>
2026-01-20 17:59:03 +0300

    feat: add the rows and columns using nested for loops

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,14 @@
 export class Gameboard{
     constructor(size){
         this.grid = []
+        for(let i = 0; i < size; i++){
+            let row = []
+            for(let j = 0; j < size; j++){
+                row.push(null)
+            }
+            this.grid.push(row)
+        }
+        
     }
 
 }

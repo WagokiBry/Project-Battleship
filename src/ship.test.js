cddaca145354081d9d0f9deadf9ebd4bb52e3fd5
Bryan <bryanwagoki2016@gmail.com>
2026-01-20 14:52:10 +0300

    test: write the necessary test to check the hits, increase the hits and check if ship is sunk

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,3 +1,48 @@
+import { ship } from './ship.js';
 
+jest.mock('./ship.js', () => {
+  return {
+    ship: jest.fn().mockImplementation(() => {
+      return {
+        length: 3,
+        hits: 0,
+        increaseHits() {
+          this.hits++;
+        },
 
+        isSunk(length, hits) {
+         
+          if (this.hits === this.length) {
+            
+            return true;
+          }
+          return false;
+        },
+      };
+    }),
+  };
+});
 
+console.log(new ship());
+
+test('ship starts with zero hits', () => {
+  let testShip = new ship();
+  expect(testShip.hits).toBe(0);
+});
+
+test('increasing hits method', () => {
+  let testShip = new ship();
+  testShip.increaseHits();
+  // expect(testShip.increaseHits()).toHaveBeenCalledTimes(1);
+  expect(testShip.hits).toBe(1);
+  
+});
+
+test('checking if ship is sunk', () => {
+  let testShip = new ship();
+  testShip.increaseHits();
+  testShip.increaseHits();
+  testShip.increaseHits();
+  
+  expect(testShip.isSunk()).toBeTruthy();
+});

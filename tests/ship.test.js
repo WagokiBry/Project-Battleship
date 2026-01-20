b0ddb2cab030af2f28c2f9a4e646673d42078f52
Bryan <bryanwagoki2016@gmail.com>
2026-01-20 15:50:21 +0300

    refactor: update the methods and properties that will be tested with proper names

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -2,24 +2,24 @@ import { ship } from '../src/ship.js';
 
 
 
-test('ship starts with zero hits', () => {
+test('ship starts with zero hit', () => {
   let testShip = new ship();
   expect(testShip.hits).toBe(0);
 });
 
-test('increasing hits method', () => {
+test('increasing hit method', () => {
   let testShip = new ship();
-  testShip.increaseHits();
-  // expect(testShip.increaseHits()).toHaveBeenCalledTimes(1);
+  testShip.hit();
+
   expect(testShip.hits).toBe(1);
   
 });
 
 test('checking if ship is sunk', () => {
   let testShip = new ship();
-  testShip.increaseHits();
-  testShip.increaseHits();
-  testShip.increaseHits();
+  testShip.hit();
+  testShip.hit();
+  testShip.hit();
   
   expect(testShip.isSunk()).toBeTruthy();
 });

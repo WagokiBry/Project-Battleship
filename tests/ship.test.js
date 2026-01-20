b510a4d217d8999083b1d598e2dd1d1332eff78c
Bryan <bryanwagoki2016@gmail.com>
2026-01-20 17:05:57 +0300

    feat: add empty files for the gameboard tests and logic

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -3,23 +3,24 @@ import { Ship } from '../src/ship.js';
 
 
 test('Ship starts with zero hit', () => {
-  let testShip = new Ship();
+  let testShip = new Ship(3);
   expect(testShip.hits).toBe(0);
 });
 
 test('increasing hit method', () => {
-  let testShip = new Ship();
+  let testShip = new Ship(3);
   testShip.hit();
 
   expect(testShip.hits).toBe(1);
   
 });
 
+
+
 test('checking if Ship is sunk', () => {
-  let testShip = new Ship();
+  let testShip = new Ship(3);
   testShip.hit();
-  testShip.hit();
-  testShip.hit();
-  
-  expect(testShip.isSunk()).toBeTruthy();
-});
+expect(testShip.isSunk()).toBeFalsy();
+  });
+
+

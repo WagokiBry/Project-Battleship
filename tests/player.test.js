15b525ccf44a0a14641b6cd2083f99807db1b653
Bryan <bryanwagoki2016@gmail.com>
2026-01-21 19:39:15 +0300

    test: add test to check whether player can attack enemy board

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,9 +1,23 @@
 
 import { Player } from "../src/player.js"
+import { Ship } from "../src/ship.js"
+import { Gameboard } from "../src/gameboard.js"
 
 test("test if player exists with a gameboard",()=>{
     let player1 = new Player()
     
 
     expect(player1.gameboard).toBeTruthy()
+})
+
+test("player can attack enemy gameboard",()=>{
+    let player1Board = new Player();
+   
+
+    let enemyPlayer = new Player()
+    let enemyShip = new Ship(3)
+    enemyPlayer.gameboard.placeShip(enemyShip,5,6,"horizontal")
+
+    player1Board.attack(enemyPlayer,5,6)
+    expect(enemyShip.hits).toBe(1)
 })

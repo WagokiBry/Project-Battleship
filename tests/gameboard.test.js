5240d8b55386d4b9a9df091a509186b5335eb2ba
Bryan <bryanwagoki2016@gmail.com>
2026-01-21 04:22:39 +0300

    test: add a test for placing the ship horizontally

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,8 +1,18 @@
 import { Gameboard } from "../src/gameboard.js";
+import { Ship } from "../src/ship.js";
 
 test("gameboard exists",()=>{
     let gameboard = new Gameboard(10)
     expect(gameboard.grid.length).toBe(10);
     expect(gameboard.grid[0].length).toBe(10);
     expect(gameboard.grid[0][0]).toBeNull();
+})
+
+test("placing a ship horizontally on the board",()=>{
+    let gameboard = new Gameboard(10);
+    let ship = new Ship(3);
+    gameboard.placeShip(ship,0,0,"horizontal")
+    expect(gameboard.grid[0][0]).toEqual(ship)
+    expect(gameboard.grid[0][1]).toEqual(ship)
+    expect(gameboard.grid[0][2]).toEqual(ship)
 })

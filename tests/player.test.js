a89745b96a8d8976c2e3b036b9b7da2bcee6871f
Bryan <bryanwagoki2016@gmail.com>
2026-01-21 10:38:06 +0300

    test: add test to check if player exists with a gameboard

---
+++ tests/player.test.js
@@ -0,0 +1,9 @@
+import { Gameboard } from "../src/gameboard"
+import { Player } from "../src/player.js"
+
+test("test if player exists with a gameboard",()=>{
+    let player1 = new Player()
+    
+
+    expect(player1.gameboard).toBeTruthy()
+})
